3c50d40b4c1ab0c156f58f827ef61d4a430b7826
Anatolie Molosag <anatolie.molosag94@gmail.com>
2022-05-13 00:32:22 -0500

    com

--- src/main/java/MentoringWithAhmet/techlistPractice.java
+++ src/main/java/MentoringWithAhmet/techlistPractice.java
@@ -27,15 +27,13 @@ public class techlistPractice {
 
 //motherfucker
 //yes
-<<<<<<< HEAD
-        //////
-=======
+
         //
 
 //no
 //motherfucker
 ////
 
->>>>>>> 2a52fa8e4aba82e0ab30371e41bb01e6619c4f5f
+
     }
 }
